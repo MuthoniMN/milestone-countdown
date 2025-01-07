9761dde5652e91c5f489a50b12321e002efdbeb0
Mitch <ndianguimichelle@gmail.com>
2025-01-07 16:41:42 +0300

    feat: nav header

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <nav className="bg-white text-[#3d016f] px-4 py-2 rounded-b-md min-w-[240px]">
+      <h1 className="text-xl">Rhedge Studios Intern Bootcamp - Milestone Countdown</h1>
+    </nav>
+  )
+}
